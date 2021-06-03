030da3396f84dd8e5c8b62833c66dd3470fad4d9
cesar.castro <cesar.castro@grupomateus.com.br>
2021-06-03 18:17:39 -0300

    habilitando cors

--- src/main/java/br/com/ionicmelhorlinguagem/api/config/SecurityConfig.java
+++ src/main/java/br/com/ionicmelhorlinguagem/api/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package br.com.ionicmelhorlinguagem.api.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.ionicmelhorlinguagem.api.security.JwtConfigurer;
 import br.com.ionicmelhorlinguagem.api.security.JwtTokenProvider;
@@ -31,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.httpBasic().disable()
-		.cors().disable()
+			.cors().and()
 			.csrf().disable().sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
 			.antMatchers("/login").permitAll()
